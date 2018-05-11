8706f857fc923182e796e03dddc8c9f8a7843118
aluno do unilasalle <alunounilasalle@LB304UNI.ACADEMICO>
2018-05-10 21:42:04 -0300

    Json

---
+++ j.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+nome = input ("nome?\n")
+user = requests.get("https://jsonplaceholder.typicode.com/users")
+req = requests.get("https://jsonplaceholder.typicode.com/posts")
+list = json.loads(req.text)
+listnome = json.loads(user.text)
+for x in listnome:
+	if (nome == x['username']):
+		id = x['id']
+		for i in list:
+			if (id == i['userId']):
+				print ("\nTitle:"+"\n"+i['title']+"\n")
+				print ("Body:"+"\n"+i['body'])
+				print ("\n")
+		break
+	else: 
+		print("Tem não")
+		break
